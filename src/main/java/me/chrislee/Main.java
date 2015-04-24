6ba72841a67543186f0c7debde653cead9e9ebfe
Abner Lee <657143946@qq.com>
2015-04-24 15:07:49 +0800

    添加一个Main.java, 仅仅是一个HelloWorld

---
+++ src/main/java/me/chrislee/Main.java
@@ -0,0 +1,11 @@
+package me.chrislee;
+
+/**
+ * Created by ChrisLee on 15-4-24.
+ * just a Main.java
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Project Management !");
+    }
+}
