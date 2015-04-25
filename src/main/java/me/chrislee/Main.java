08e3ccd303100af455b90c78100db456f186e191
Abner Lee <657143946@qq.com>
2015-04-25 10:47:04 +0800

    编写简单的Controller 测试了springMVC，同时测试了自定义的视图渲染器

--- src/main/java/me/chrislee/Main.java
+++ src/main/java/me/chrislee/Main.java
@@ -1,10 +1,33 @@
 package me.chrislee;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
 /**
  * Created by ChrisLee on 15-4-24.
  * just a Main.java
  */
+@Controller
+@RequestMapping("/main")
 public class Main {
+
+    @RequestMapping("/index")
+    public ModelAndView getHtml(@RequestParam String name){
+        ModelAndView mv = new ModelAndView();
+        /**
+         * 配置同步渲染使用的参数
+         */
+        mv.addObject("name", name);
+        /**
+         * 设置页面
+         */
+        mv.setViewName("/main.html");
+        return mv;
+
+    }
+
     public static void main(String[] args){
         System.out.println("Project Management !");
     }
