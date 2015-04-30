bc340d4622f6a15f87ec9da7c43f049841ef9ff9
Abner Lee <657143946@qq.com>
2015-04-30 11:20:12 +0800

    实体

---
+++ src/main/java/me/chrislee/database/entity/LoginEntity.java
@@ -0,0 +1,35 @@
+package me.chrislee.database.entity;
+
+/**
+ * Created by ChrisLee on 15-4-25.
+ * 登陆表
+ */
+public class LoginEntity {
+    private int id;
+    private String email;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
