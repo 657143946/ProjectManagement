1a072f5f1c1947095116a3ad0e6f0291d79963b9
Abner Lee <657143946@qq.com>
2015-05-09 16:17:37 +0800

    修改构造方法，不能传入data字段，同时增加了addAll方法

--- src/main/java/me/chrislee/controller/DemoModel.java
+++ src/main/java/me/chrislee/controller/DemoModel.java
@@ -1,5 +1,6 @@
 package me.chrislee.controller;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -9,16 +10,19 @@ import java.util.Map;
 public class DemoModel {
     private boolean flag = true;
     private String msg = "";
-    private Map<String, Object> data = null;
+    private Map<String, Object> data = new HashMap<>();
 
-    public DemoModel(boolean flag, String msg, Map<String, Object> data){
+    public DemoModel(boolean flag, String msg) {
         this.flag = flag;
         this.msg = msg;
-        this.data = data;
     }
 
-    public void add(String key, Object value){
+    public void add(String key, Object value) {
         data.put(key, value);
+    }
+
+    public void addAll(Map<String, Object> params) {
+        data.putAll(params);
     }
 
     public boolean isFlag() {
