54105ff5abdc084970e697a10023b41b1259d8e7
Abner Lee <657143946@qq.com>
2015-05-12 17:37:30 +0800

    登陆表的实体

--- src/main/java/me/chrislee/database/entity/LoginEntity.java
+++ src/main/java/me/chrislee/database/entity/LoginEntity.java
@@ -1,13 +1,24 @@
 package me.chrislee.database.entity;
 
+import java.sql.Timestamp;
+
 /**
  * Created by ChrisLee on 15-4-25.
  * 登陆表
  */
 public class LoginEntity {
     private int id;
+    private String userName;
     private String email;
     private String password;
+    private String sessionId;
+    private Timestamp createTimestamp;
+    private String lastLoginIp;
+    private Timestamp lastLoginTimestamp;
+    private int level;
+
+    private boolean active;
+    private String activeCode;
 
     public int getId() {
         return id;
@@ -15,6 +26,14 @@ public class LoginEntity {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 
     public String getEmail() {
@@ -31,5 +50,61 @@ public class LoginEntity {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public Timestamp getCreateTimestamp() {
+        return createTimestamp;
+    }
+
+    public void setCreateTimestamp(Timestamp createTimestamp) {
+        this.createTimestamp = createTimestamp;
+    }
+
+    public String getLastLoginIp() {
+        return lastLoginIp;
+    }
+
+    public void setLastLoginIp(String lastLoginIp) {
+        this.lastLoginIp = lastLoginIp;
+    }
+
+    public Timestamp getLastLoginTimestamp() {
+        return lastLoginTimestamp;
+    }
+
+    public void setLastLoginTimestamp(Timestamp lastLoginTimestamp) {
+        this.lastLoginTimestamp = lastLoginTimestamp;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getActiveCode() {
+        return activeCode;
+    }
+
+    public void setActiveCode(String activeCode) {
+        this.activeCode = activeCode;
     }
 }
