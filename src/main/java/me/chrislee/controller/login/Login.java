a54e2deb88d1327ce18c6ca727244959e7182bc5
Abner Lee <657143946@qq.com>
2015-05-15 09:46:04 +0800

    增加登陆的Controller

---
+++ src/main/java/me/chrislee/controller/login/Login.java
@@ -0,0 +1,55 @@
+package me.chrislee.controller.login;
+
+import me.chrislee.controller.DemoModel;
+import me.chrislee.database.entity.LoginEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by ChrisLee on 15-5-12.
+ * 注册登录当账号操作的控制器
+ */
+@Controller
+@RequestMapping("/login")
+public class Login {
+    /**
+     * 首页页面，根据用户是否登陆决定是否跳转首页
+     */
+    @RequestMapping("/index.page")
+    public ModelAndView index(HttpServletRequest request) {
+        LoginEntity login = (LoginEntity) request.getSession().getAttribute("login");
+        if (login == null) {
+            /**
+             * 未登录情况下，显示登录首页
+             */
+            ModelAndView mv = new ModelAndView();
+            mv.setViewName("login/login.html");
+            return mv;
+        } else {
+            /**
+             * 已经登录情况，根据逻辑需求跳转
+             */
+            ModelAndView mv = new ModelAndView();
+            mv.setViewName("login/login.html");
+            return mv;
+        }
+    }
+
+    @RequestMapping("/login.data")
+    public DemoModel login() {
+        DemoModel ret = new DemoModel(true, "SUCCESS");
+        return ret;
+    }
+
+    @RequestMapping("/register.data")
+    public DemoModel register(){
+        DemoModel ret = new DemoModel(true, "SUCCESS");
+        return ret;
+    }
+
+
+
+}
