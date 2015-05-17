5c2d93da2ef8478103d09846142d20a3a87dc537
Abner Lee <657143946@qq.com>
2015-05-17 10:21:01 +0800

    修改login dao的bug

--- src/main/java/me/chrislee/database/dao/LoginDao.java
+++ src/main/java/me/chrislee/database/dao/LoginDao.java
@@ -2,42 +2,19 @@ package me.chrislee.database.dao;
 
 
 import me.chrislee.database.entity.LoginEntity;
-import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
 /**
  * Created by ChrisLee on 15-4-25.
- *
+ * 针对Login 数据逻辑的DAO
  */
 @Repository
 public interface LoginDao {
     /**
-     * 通过 id 查询
-     * 返回一条记录
-     */
-    @Select("select * from login where id=#{id}")
-    public LoginEntity findById(int id);
-
-    /**
-     * 通过 email 查询
-     * 返回一条记录
-     */
-    @Select("select * from login where email=#{email}")
-    public LoginEntity findByEmail(String email);
-
-    /**
      * 插入login记录
      * 返回插入条数
      */
-    @Insert("insert into login(id, email, password) values(#{id}, #{email}, #{password})")
+    @Insert("insert into login(id, userName, email, password, sessionId, lastLoginIp, lastLoginTimestamp, level, isActive, activeCode) " +
+            "values(#{id}, #{userName}, #{email}, #{password}, #{sessionId}, #{lastLoginIp}, #{lastLoginTimestamp}, #{level}, #{isActive}, #{activeCode})")
     public int save(LoginEntity login);
-
-    @Delete("delete from login where id=#{id}")
-    public int deleteById(int id);
-
-    public List<LoginEntity> findAll();
 }
