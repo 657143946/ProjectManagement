6992bdaffea70e43ab44aa5aefd2183d2baa00d5
Abner Lee <657143946@qq.com>
2015-05-17 10:19:20 +0800

    添加service层的注册逻辑

--- src/main/java/me/chrislee/database/service/LoginService.java
+++ src/main/java/me/chrislee/database/service/LoginService.java
@@ -1,22 +1,45 @@
 package me.chrislee.database.service;
 
+import me.chrislee.constant.user.UserLevel;
 import me.chrislee.database.entity.LoginEntity;
+import me.chrislee.utils.cipher.CipherUtils;
+import me.chrislee.utils.id.IdUtils;
 import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
 
 /**
  * Created by ChrisLee on 15-5-9.
- *
  */
 @Service
-public class LoginService extends BaseService{
-    public void testTransaction(){
-        for (int id=1; id<10; id++){
-            LoginEntity login = new LoginEntity();
-            login.setId(id);
-            login.setEmail("email_"+id);
-            login.setPassword("password_"+id);
-            getLoginDao().save(login);
-        }
+public class LoginService extends BaseService {
+
+    /**
+     * 注册逻辑
+     *
+     * @param userName 用户名
+     * @param email    邮箱
+     * @param password 原始密码（未加密）
+     * @param level    用户级别
+     * @param ip       IP
+     * @return 登陆实体类
+     */
+    public LoginEntity register(String userName, String email, String password, UserLevel level, String ip) {
+        LoginEntity login = new LoginEntity();
+
+        login.setUserName(userName);
+        login.setEmail(email);
+        login.setPassword(CipherUtils.md5(password, 10));
+        login.setSessionId(IdUtils.getId());
+        login.setLastLoginIp(ip);
+        login.setLastLoginTimestamp(new Timestamp(System.currentTimeMillis()));
+        login.setLevel(level.getLevel());
+        login.setActive(false);
+        login.setActiveCode(IdUtils.getId());
+
+        this.getLoginDao().save(login);
+
+        return login;
     }
 
 }
